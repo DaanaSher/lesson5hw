d46d3225da174ff603c85b7cc31acec3708ca427
daana <daanasher000@gmail.com>
2021-11-16 22:24:44 +0600

    lesson5hw

---
+++ src/com/company/HomeWork5/Main.java
@@ -0,0 +1,11 @@
+package com.company.HomeWork5;
+
+public class Main {
+    public static void main(String[] args) {
+        Boss boss = new Boss();
+        boss.setDamage(50);
+        boss.setHealth(700);
+        boss.setDefenceType("Physical");
+        System.out.println(boss.getInfo());
+    }
+}
